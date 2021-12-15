f852924585b06967e3ed10800f9a59b68fe5513d
Bardoctorus <ian.m.buckley@gmail.com>
2021-12-15 16:13:23 +0100

    Basic step through of current idea in a comment

--- day3/part2/main.c
+++ day3/part2/main.c
@@ -8,11 +8,13 @@ int binToDec(int n);
 //MAIN___________________________________
 int main(int argc,char ** argv) 
 {
+	//ARGS HANDLING
 	if(argc < 2)
 	{
 		printf("Need a filename arg\n");
 		exit(-1);
 	}
+	//DECLARATIONS
 	char * filename = argv[1];
 	FILE *fp;
 	fp = fopen(filename, "r");
@@ -30,6 +32,7 @@ int main(int argc,char ** argv)
 	int zeros = 0;
 	char gamma[64];
 	char epsilon[64];
+	//INPUT
 	while (fgets(str, 100, fp))
 	{
 		for(int i = 0; i < strlen(str); i ++)
@@ -46,7 +49,17 @@ int main(int argc,char ** argv)
 		pstr = &str[0];
 		counter++;
 	}
-	printf("counter is %d\n",counter);
+//	printf("counter is %d\n",counter);
+//
+//COUNTING BITS
+//
+//
+//Here we are stepping through counting bits
+//once we count them we get a result variable.
+//using this result variable we need to walk through the input again, keeping the significant bit entries and discarding others
+//if memory is cheap, we just make a new array each time
+//if it isn't, we need to do pointer fuckery and keep track of where everything is ourselves which sounds like hell...
+//... cool lets do the second one.
 	for (int k = 0; k < strlen(str)-1; k++)
 	{
 		for (int l = 0; l < counter+1; l++)
@@ -62,6 +75,14 @@ int main(int argc,char ** argv)
 //			printf("inputs[%d][%d] is %d\n",k,l,(inputs[l][k] - '0'));
 		}
 		int result = (ones>zeros);
+		// TODO step through input[][] again keeping track of which ones to keep, and which ones to ditch
+		// maybe the best way to do it is:
+		//  - for [0][x] Go until find first 0 
+		//  - forward once - if its a !0 (this is DANGER, handle \o) copy it to first 0 position, else keep going
+		//  - do this once for every element in the array.
+		//  - go through and put an '\0' at the new end.
+		//  - repeat for [1][x] and happy days? riiight?!? 
+		//  - if it works, do teh exact same thing in reverse for the one that nees a 0. Make a function that can do either as an arg.
 		gamma[k] = (result+'0');
 		epsilon[k] = (!result + '0');
 //		printf("ones is %d, zeros is %d, result is %d,Gamma[%d] = %d\n",ones,zeros,result,k, result);
