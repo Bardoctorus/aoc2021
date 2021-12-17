4830ce10471a56b3b1fcc8c8038b613b391d6dfb
Bardoctorus <ian.m.buckley@gmail.com>
2021-12-17 21:11:01 +0100

    I thought I was being clever and had come up with a super generic way to trot through any array and return the significant bits but it turns out I dont get array pointers after all

--- day3/part2/main.c
+++ day3/part2/main.c
@@ -3,9 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 //DECS_________________________________
-int ctoi(int i);
-int binToDec(int n);
-void arrayShifter(char * arrayIn, char * arrayOut, int sigBit);
+
+int sigBitAtPosition(char * input, int step, int offset);
 //MAIN___________________________________
 int main(int argc,char ** argv) 
 {
@@ -31,10 +30,7 @@ int main(int argc,char ** argv)
 	int counter = 0;
 	int ones = 0;
 	int zeros = 0;
-	char gamma[64];
-//
-//char epsilon[64];
-	//INPUT
+//INPUT
 	while (fgets(str, 100, fp))
 	{
 		for(int i = 0; i < strlen(str); i ++)
@@ -51,22 +47,12 @@ int main(int argc,char ** argv)
 		pstr = &str[0];
 		counter++;
 	}
-//	printf("counter is %d\n",counter);
-//
-//COUNTING BITS
-//
-//
-//Here we are stepping through counting bits
-//once we count them we get a result variable.
-//using this result variable we need to walk through the input again, keeping the significant bit entries and discarding others
-//if memory is cheap, we just make a new array each time
-// actually thats whack you just need two new arrays
-	
-		char thing[]="Bingo";
+	sigBitAtPosition(inputs[0],strlen(str),0);
 	for (int k = 0; k < strlen(str)-1; k++)
 	{
-		for (int l = 0; l < counter+1; l++)
+		for (int l = 0; l < counter; l++)
 		{
+			//printf("Value at inputs[%d][%d] by pointer: %c\n",l,k,*(inputs[l]+1));
 			if((inputs[l][k] - '0') == 0)
 			{
 				zeros++;
@@ -75,69 +61,22 @@ int main(int argc,char ** argv)
 			{
 				ones++;		
 			}
-//			printf("inputs[%d][%d] is %d\n",k,l,(inputs[l][k] - '0'));
 		}
-		int result = (ones>zeros);
-		// TODO step through input[][] again keeping track of which ones to keep, and which ones to ditch
-		// maybe the best way to do it is:
-		//  - for [0][x] Go until find first 0 
-		//  - forward once - if its a !0 (this is DANGER, handle \o) copy it to first 0 position, else keep going
-		//  - do this once for every element in the array.
-		//  - go through and put an '\0' at the new end.
-		//  - repeat for [1][x] and happy days? riiight?!? 
-		//  - if it works, do teh exact same thing in reverse for the one that nees a 0. Make a function that can do either as an arg.
-		gamma[k] = (result+'0');
-		//epsilon[k] = (!result + '0');
-		arrayShifter(gamma, thing, result);
-//		printf("ones is %d, zeros is %d, result is %d,Gamma[%d] = %d\n",ones,zeros,result,k, result);
+//		int result = (ones>zeros);
 		ones = 0;
 		zeros = 0;
-//		printf("gamma[%d] is %c\n",k,(gamma[k]));
 	}
-//	gamma[strlen(str)-1] = '\0';
-//	epsilon[strlen(str)-1] = '\0';
-//	printf("Gamma in full:START %s END\n\n",gamma);
-//	printf("Epsilon in full:START %s END\n\n",epsilon);
-//	char *endptr;
-//	long int gamInt =strtol(gamma,&endptr,2);
-//	long int epInt = strtol(epsilon,&endptr, 2);
-//	printf("gamint = %ld, epint %ld\n", gamInt,epInt);
-////	printf("Gamma bintodec:  %d, Epsilon bin2dec: %d\n",binToDec(gamInt),binToDec(epInt));
-//	int power =gamInt * epInt;
-//	printf("Final power reading: %d\n",power);
 	fclose(fp);
 	return 0;
 }
 //FUNCS_________________________________________
-int ctoi(int i)
+int sigBitAtPosition(char * input, int step,int offset)
 {
-	return i-'0';
-}
-
-int binToDec(int n)
-{
-	int dec_val = 0;
-
-	int base = 1;
-
-	int temp = n;
-
-	while (temp) 
+	int x = 0;
+	while(input[x] != '\0')
 	{
-		int last = temp % 10;
-		temp = temp/10;
-
-		dec_val += last * base;
-		base  = base * 2;
+		printf("char here: %c\n",*(&input[x]+offset+1));
+		x+=step;
 	}
-	return dec_val;
-}
-void arrayShifter(char* arrayIn, char * arrayOut, int sigBit)
-{
-	// This shit here. It:
-	//  - 1. Takes an array to work through
-	printf("This is arrayIn: %s, this is arrayOut %s, this is sigBit:%d\n",arrayIn,arrayOut,sigBit);
-	//  - 2. Goes through the first bits, sorting by the sigBit
-	//  - 3. puts those in the out arrayOut
-	//  - 4. Goes back to 1 but with the Arrayout as the input array, stops when  
-}
+	return 0;
+}	
